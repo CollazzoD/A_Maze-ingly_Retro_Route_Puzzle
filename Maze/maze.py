284385b68138eb8e7afb9b51623a31b85a202d9f
CollazzoD <collazzo.d@gmail.com>
2021-11-21 17:46:42 +0100

    Add fromJson with test

--- Maze/maze.py
+++ Maze/maze.py
@@ -1,6 +1,17 @@
+import json
+import logging
+
 class Maze():
     def __init__(self):
-        self.graph = None
+        self.graph = {}
+        self.cardinals = ['north', 'south', 'west', 'east']
 
-    def fromJson(filename):
-        pass
+    def fromJson(self, json_filename):
+        with open(json_filename) as json_file:
+            json_map = json.loads(json_file.read())
+            for room in json_map['rooms']:
+                neighbours = []
+                for cardinal in self.cardinals:
+                    if cardinal in room:
+                        neighbours.append(room[cardinal])
+                self.graph[room['id']] = neighbours
--- test/test_maze.py
+++ test/test_maze.py
@@ -9,11 +9,36 @@ class TestMaze(unittest.TestCase):
 
     def test_maze_class_graph_init_value(self):
         m = Maze()
-        self.assertIsNone(m.graph)
+        self.assertDictEqual(m.graph, {})
 
     def test_maze_class_fromJson_method_existence(self):
         m = Maze()
         self.assertTrue(hasattr(m, 'fromJson'))
+
+    def test_maze_class_fromJson_method_with_map_1(self):
+        test_graph = {
+            1 : [2],
+            2 : [1, 3, 4],
+            3 : [2],
+            4 : [2]
+        }
+        m = Maze()
+        m.fromJson('json_files/map1.json')
+        self.assertDictEqual(m.graph, test_graph)
+
+    def test_maze_class_fromJson_method_with_map_2(self):
+        test_graph = {
+        1 : [2, 7],
+        2 : [5, 1, 3, 4],
+        3 : [2],
+        4 : [6, 7, 2],
+        5 : [2, 6],
+        6 : [4, 5],
+        7 : [4, 1]
+        }
+        m = Maze()
+        m.fromJson('json_files/map2.json')
+        self.assertDictEqual(m.graph, test_graph)
 
 if __name__ == '__main__':
     unittest.main()
